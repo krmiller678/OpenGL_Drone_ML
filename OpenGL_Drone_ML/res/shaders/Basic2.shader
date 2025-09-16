3fc325a4716f1797271d6131d37e582803c64759
krmiller678 <krmiller678@gmail.com>
2025-09-16 10:45:05 -0400

    Created basic shader 2 for bigger vertex buffer. Set update for view matrix on simulation side.

---
+++ OpenGL_Drone_ML/res/shaders/Basic2.shader
@@ -0,0 +1,31 @@
+#shader vertex
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
+
+out vec3 ourColor;
+out vec2 TexCoord;
+
+uniform mat4 u_MVP;
+
+void main()
+{
+   gl_Position = u_MVP * vec4(aPos, 1.0);
+   ourColor = aColor;
+   TexCoord = aTexCoord;
+}
+
+#shader fragment
+#version 330 core
+out vec4 FragColor;
+  
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D u_Texture;
+
+void main()
+{
+   FragColor = texture(u_Texture, TexCoord);
+}
--- OpenGL_Drone_ML/tests/Test2DMultiTexture.cpp
+++ OpenGL_Drone_ML/tests/Test2DMultiTexture.cpp
@@ -19,37 +19,39 @@ namespace test
           m_TranslationA(200, 200, 0)
     {
         // target positions
-        m_Targets.push_back({800, 400, 0});
-        m_Targets.push_back({50, 50, 0});
-        m_Targets.push_back({400, 200, 0});
+        m_Targets.push_back({480, 135, 0});
+        m_Targets.push_back({960, 135, 0});
+        m_Targets.push_back({1440, 135, 0});
+        m_Targets.push_back({480, 405, 0});
 
         float positions[] = {
-            -50.0f, -50.0f, 0.0f, 0.0f, // 0
-            50.0f, -50.0f, 1.0f, 0.0f,  // 1
-            50.0f, 50.0f, 1.0f, 1.0f,   // 2
-            -50.0f, 50.0f, 0.0f, 1.0f   // 3
+            // positions             // colors           // texture coords
+            -50.0f, -50.0f,  1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, // 0
+             50.0f, -50.0f,  1.0f,   0.0f, 0.0f, 0.0f,   1.0f, 0.0f,  // 1
+             50.0f,  50.0f,  1.0f,   0.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 2
+            -50.0f,  50.0f,  1.0f,   0.0f, 0.0f, 0.0f,   0.0f, 1.0f   // 3
         };
 
         unsigned int indices[] = {
             0, 1, 2,
             2, 3, 0};
 
-        GLCall(glEnable(GL_BLEND));
+        GLCall(glEnable(GL_BLEND));  
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
         m_VAO = std::make_unique<VertexArray>();
 
-        m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float));
+        m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 4 * 8 * sizeof(float));
         VertexBufferLayout layout;
-        layout.Push<float>(2);
+        layout.Push<float>(3);
+        layout.Push<float>(3); // 3 floats for color
         layout.Push<float>(2); // Added 2 more floats for each vertex texture coordinates
         m_VAO->AddBuffer(*m_VertexBuffer, layout);
 
         m_IndexBuffer = std::make_unique<IndexBuffer>(indices, 6);
 
-        m_Shader = std::make_unique<Shader>("res/shaders/Basic.shader");
+        m_Shader = std::make_unique<Shader>("res/shaders/Basic2.shader");
         m_Shader->Bind();
-        m_Shader->SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
         m_Texture = std::make_unique<Texture>("res/textures/alien.png");
         m_Texture2 = std::make_unique<Texture>("res/textures/casa.png");
@@ -66,7 +68,7 @@ namespace test
 
     void Test2DMultiTexture::OnRender()
     {
-        GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+        GLCall(glClearColor(0.0f, 0.5f, 0.0f, 1.0f));
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
         Renderer renderer;
@@ -143,8 +145,17 @@ namespace test
             std::cout << "Next action: " << action.dump() << std::endl;
 
             // Update sim
-            m_TranslationA.x = action["x"];
-            m_TranslationA.y = action["y"];
+            if (action["x"] > 910)
+            {
+                m_View = glm::translate(m_View, glm::vec3(m_TranslationA.x - float(action["x"]), 0, 0));
+                m_TranslationA.y = action["y"];
+            }
+            else
+            {
+                m_TranslationA.x = action["x"];
+                m_TranslationA.y = action["y"];
+            }
+            
         }
         catch (const std::exception &e)
         {
